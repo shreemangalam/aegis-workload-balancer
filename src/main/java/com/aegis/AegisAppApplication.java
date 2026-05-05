e94091ced1be5b7a7090b35c59ebe170a0e4b82d
shreemangalam <singhshreemangalam@gmail.com>
2026-05-05 23:57:48 +0530

    chore: initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/aegis/AegisAppApplication.java
@@ -0,0 +1,13 @@
+package com.aegis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aegis/AegisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
